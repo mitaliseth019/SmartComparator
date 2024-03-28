4a16570f0600e5505c9555fe81a330a825fa7e6f
Mitali Seth <mitali.seth@walmart.com>
2024-03-28 10:36:56 +0530

    1st api with comman feature

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from Product import Product
 import Repository
 from datetime import datetime
 import json
+from callGpt import GPTModelClient
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///product.db'
@@ -24,14 +25,28 @@ def get_product():
     retrieved_product1 = Repository.getProduct(product_id1)
     retrieved_product2 = Repository.getProduct(product_id2)
     result = []
+    product1_features=json.loads(retrieved_product1.features)
+    product2_features=json.loads(retrieved_product2.features)
+
+    if retrieved_product1.features and retrieved_product2.features:
+       common_features= set(product1_features) & set(product2_features)
     if retrieved_product1:
         print("retrieved_product1  ", retrieved_product1)
-        product1 = Product(id=retrieved_product1.productId, name=retrieved_product1.name, features=json.loads(retrieved_product1.features), price=retrieved_product1.price)
+        product1 = Product(id=retrieved_product1.productId, name=retrieved_product1.name, features={key: product1_features[key] for key in common_features}, price=retrieved_product1.price)
         result.append(product1)
     if retrieved_product2:
         print("retrieved_product2  ", retrieved_product2)
-        product2 = Product(id=retrieved_product2.productId, name=retrieved_product2.name, features=json.loads(retrieved_product2.features), price=retrieved_product2.price)
+        product2 = Product(id=retrieved_product2.productId, name=retrieved_product2.name, features={key: product2_features[key] for key in common_features}, price=retrieved_product2.price)
         result.append(product2)
     return json.dumps([res.__dict__ for res in result])
+
+@app.route('/getFeatureDetails', methods=['GET'])
+def get_feature():
+    featureKey = request.args.get('feature')
+    feature1 = request.args.get('featureProd1')
+    feature2 = request.args.get('featureProd2')
+    gpt = GPTModelClient()
+
+
 if __name__ == '__main__':
     app.run(debug=True)
